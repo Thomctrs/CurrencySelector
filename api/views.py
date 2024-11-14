2ca1fa0929cb943c81e5825beb71528007b50624
Noé Chabanon <noe.chabanon@gmail.com>
2024-11-14 13:19:29 +0100

    endpoint load csv

--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
+import pandas as pd
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
 
 from rest_framework import generics, permissions
 from .models import Devise
@@ -16,3 +21,18 @@ class DeviseListView(generics.ListAPIView):
     http_method_names = ['get']
 
 
+
+@csrf_exempt
+def upload_excel(request):
+    if request.method == "POST" and request.FILES.get("file"):
+        excel_file = request.FILES["file"]
+        file_path = default_storage.save("temp.xlsx", excel_file)
+
+        # Charger le fichier Excel avec pandas
+        df = pd.read_excel(file_path)
+
+       # date_str, exchange_rate_str = line.split(",")
+
+       # return JsonResponse({"data": data})
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
